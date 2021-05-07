441dd5dcfece82b85bd83df6adc5a2123dbf50a9
demo <aa@123.com>
2021-05-07 16:11:30 +0800

    restore gulp

---
+++ gulp/autoBuild/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css'),
+    stripDebug = require('gulp-strip-debug'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    del = require('del');
+    Vinyl = require('vinyl');
+
+
+
+gulp.task('scss', function() {
+    return gulp.src('public/static/css/**/*.scss')
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('public/static/css'));
+});
+gulp.task('js', function() {
+    return gulp.src('public/static/js/**/*.js')
+        .pipe(stripDebug())
+        .pipe(uglify({ mangle: {except: ['require' ,'exports' ,'module' ,'$']}, compress: true }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/static/js'));
+});
+gulp.task('clean:js', async() => {
+    await del([
+        'public/static/css/**/*.scss',
+        'public/static/js/**/*.js',
+        '!public/static/js/**/*.min.js'
+    ]);
+
+})
+gulp.task('default', gulp.series(gulp.parallel('scss', 'js'), 'clean:js'));
+
+// gulp.task('watch', function() {
+//     gulp.watch('static/**/*.{js,scss}').on('change', function(e) {
+//         console.log(e);
+//         exec('ifconfig', function(err, stdout, stderr) {
+//             console.log(stdout);
+//             console.logs(stderr);
+//             console.error(err);
+//         });
+//     });
+// });
